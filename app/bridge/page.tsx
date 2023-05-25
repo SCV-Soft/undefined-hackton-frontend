aaffbde1edaadf0b86ed3ee598767502f65c3fd9
2-NOW <kj109888@gmail.com>
2023-05-26 06:10:37 +0900

    Refactor BridgePage component to remove unnecessary searchParams prop and update Header subtitle for clarity

--- app/bridge/page.tsx
+++ app/bridge/page.tsx
@@ -1,34 +1,25 @@
 import { FaSync } from "react-icons/fa";
 
 import { BridgeSwap } from "components/app/bridge";
-import { Layer1Swap } from "components/app/layer1";
 import { Header } from "components/common";
 
-interface Layer1PageProps {
-  searchParams: {
-    target: string;
-  };
-}
-
-export default function BridgePage({ searchParams }: Layer1PageProps) {
-  const target = searchParams?.target?.toUpperCase() ?? "";
-
+export default function BridgePage() {
   return (
     <div className="flex flex-col gap-4">
       <Header
         title={
           <div className="flex items-end gap-4">
-            <span>Layer 1 Swap</span>
+            <span>Bridge</span>
             <span className="flex items-center gap-2 text-lg">
               vETH
               <FaSync />
-              {target}
+              vETH
             </span>
           </div>
         }
-        subtitle={`Swap ${target} to vETH the next generation LSD Token`}
+        subtitle="Move your vETH to Layer 2 by one step"
       />
-      <BridgeSwap target={target} />
+      <BridgeSwap />
     </div>
   );
 }
