261003ac21c5290714c9d839fe57f197f8afcaa1
2-NOW <kj109888@gmail.com>
2023-05-26 05:10:17 +0900

    Add MyInfos component to display user's available balance and address with avatar image

--- components/common.tsx
+++ components/common.tsx
@@ -1,7 +1,14 @@
+import { generateAvatarURL } from "@cfx-kit/wallet-avatar";
 import clsx from "clsx";
-import { HTMLAttributes, PropsWithChildren, ReactNode } from "react";
+import Image from "next/image";
+import {
+  ButtonHTMLAttributes,
+  InputHTMLAttributes,
+  PropsWithChildren,
+  ReactNode,
+} from "react";
 
-interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
 }
 
@@ -19,19 +26,19 @@ export const Button = ({ label, ...props }: ButtonProps) => {
   );
 };
 
-interface InputProps extends HTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   left?: ReactNode;
   right?: ReactNode;
 }
 
 export const Input = ({ left, right, ...props }: InputProps) => {
   return (
-    <div className="flex items-center rounded-full border-2 border-black/30 px-4 shadow-md">
+    <div className="flex h-12 items-center rounded-full border-2 border-black/30 px-4 shadow-md">
       {left && <span>{left}</span>}
       <input
         className={clsx([
           "grow appearance-none outline-none",
-          "min-w-0 bg-transparent p-2 font-black",
+          "min-w-0 bg-transparent px-2 font-black",
           "placeholder:text-sm placeholder:font-semibold placeholder:text-black/30",
           props?.className,
         ])}
@@ -74,6 +81,37 @@ export const Infos = ({ data }: InfosProps) => {
           </dl>
         );
       })}
+    </div>
+  );
+};
+
+interface MyInfosProps {
+  baseSymbol: string;
+  available: string;
+  address: string;
+}
+
+export const MyInfos = ({ baseSymbol, available, address }: MyInfosProps) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-medium text-black/50">Available to stake</h1>
+        <p className="text-2xl font-semibold">
+          {available} {baseSymbol}
+        </p>
+      </div>
+      <div className="flex items-center rounded-full bg-stone-600">
+        <Image
+          className="rounded-full"
+          src={generateAvatarURL(address)}
+          width={24}
+          height={24}
+          alt="jazzicon"
+        />
+        <span className="px-2 text-xs font-semibold text-white">
+          {address.slice(0, 6)}...{address.slice(-4)}
+        </span>
+      </div>
     </div>
   );
 };
