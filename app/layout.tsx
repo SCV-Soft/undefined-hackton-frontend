fdb914467fc389cb5b8e449d2d88943d4820b0f9
2-NOW <kj109888@gmail.com>
2023-05-26 05:21:54 +0900

    Add Configs component to RootLayout to enable global configuration options

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "styles/tailwindcss.css";
 
 import { PropsWithChildren } from "react";
 
+import { Configs } from "components/global/config";
 import { GlobalLayout } from "components/global/layout";
 import { ModalContainer } from "components/global/modal/container";
 import { ToastContainer } from "components/global/toast";
@@ -16,7 +17,9 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body>
-        <GlobalLayout>{children}</GlobalLayout>
+        <Configs>
+          <GlobalLayout>{children}</GlobalLayout>
+        </Configs>
         <ToastContainer />
         <ModalContainer />
       </body>
