28b401d12f9cb4f2c5d62918ef68401c904f5955
2-NOW <kj109888@gmail.com>
2023-05-26 00:53:53 +0900

    Add GlobalLayout component with a main container, a side bar, and a content section

---
+++ components/global/layout/index.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+
+import { Side } from "./side";
+
+export const GlobalLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <main className="flex">
+      {/* side bar */}
+      <aside className="min-w-[100px]">
+        <Side />
+      </aside>
+      <section className="mx-auto pt-36">{children}</section>
+    </main>
+  );
+};
