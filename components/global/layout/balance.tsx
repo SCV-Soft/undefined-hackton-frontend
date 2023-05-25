d3bc2991f12f3fe62925dc3f4e1ff5f15176e4cd
2-NOW <kj109888@gmail.com>
2023-05-26 08:30:44 +0900

    Add useAtomValue to retrieve vETH and scETH values in Balance component

--- components/global/layout/balance.tsx
+++ components/global/layout/balance.tsx
@@ -1,14 +1,25 @@
+import { useAtomValue } from "jotai";
+
+import { SCETH, VETH } from "atom/web3/balance/state";
+
 export const Balance = () => {
+  const vETH = useAtomValue(VETH);
+  const scETH = useAtomValue(SCETH);
+
   return (
     <div className="flex flex-col gap-4">
       <dl>
         <dt className="text-black/50">Your vETH</dt>
-        <dd className="text-xl font-semibold">32.004</dd>
+        <dd className="text-xl font-semibold">
+          {vETH.length > 10 ? vETH.slice(0, 10) + "..." : vETH}
+        </dd>
       </dl>
 
       <dl>
         <dt className="text-black/50">Your scETH</dt>
-        <dd className="text-xl font-semibold">2.015</dd>
+        <dd className="text-xl font-semibold">
+          {scETH.length > 10 ? scETH.slice(0, 10) + "..." : scETH}
+        </dd>
       </dl>
     </div>
   );
