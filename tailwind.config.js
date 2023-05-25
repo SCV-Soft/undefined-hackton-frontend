9226c8993ee7e8eb6bfb263eaa73f7cd6b20f388
2-NOW <kj109888@gmail.com>
2023-05-25 15:24:54 +0900

    Extend the theme with new colors and screen sizes in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,27 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: { extend: {}, },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3454C6",
+        secondary: "#E7E2D4",
+        tertiary: "#3C3C3C",
+        base: {
+          color: "#E7E2D4",
+          font: "#3C3C3C",
+        },
+      },
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1920px",
+      }
+    },
+  },
   plugins: [require("daisyui")],
 }
