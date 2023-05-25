7b495979d265295ac3911ded8596dfab6b2bd4e7
2-NOW <kj109888@gmail.com>
2023-05-26 06:05:15 +0900

    Add Layer2 Swap page to app with Polygon (MATIC) support and ability to swap WETH to vETH LSD Token

---
+++ app/swap/layer2/page.tsx
@@ -0,0 +1,29 @@
+import { Layer2Swap } from "components/app/layer2";
+import { Header } from "components/common";
+
+interface Layer1PageProps {
+  searchParams: {
+    target: string;
+  };
+}
+
+export default function Layer1Page({ searchParams }: Layer1PageProps) {
+  const target = searchParams?.target?.toUpperCase() ?? "";
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Header
+        title={
+          <div className="flex items-end gap-4">
+            <span>Layer 2 Swap</span>
+            <span className="flex items-center gap-2 text-lg">
+              Polygon (MATIC)
+            </span>
+          </div>
+        }
+        subtitle="Swap WETH to vETH the next generation LSD Token"
+      />
+      <Layer2Swap target={target} />
+    </div>
+  );
+}
