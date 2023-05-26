7ec6a84b94b6fb5c7754f7eec4dae0d7af92bd71
2-NOW <kj109888@gmail.com>
2023-05-26 10:01:08 +0900

    Refactor Configs component to use fragment instead of LifeCycle component for better readability

--- components/global/config/index.tsx
+++ components/global/config/index.tsx
@@ -5,9 +5,9 @@ import { Web3Providers } from "./web3providers";
 
 export const Configs = ({ children }: PropsWithChildren) => {
   return (
-    <LifeCycle>
+    <>
       <Web3Providers />
-      {children}
-    </LifeCycle>
+      <LifeCycle>{children}</LifeCycle>
+    </>
   );
 };
