28d6930f538752168d09e1f6c911174dfbe6bc9f
2-NOW <kj109888@gmail.com>
2023-07-30 03:56:54 +0900

    feat: add useDeriveValue hook for value derivation

---
+++ hooks/useDeriveValue.ts
@@ -0,0 +1,36 @@
+import ky from "ky";
+import useSWR from "swr";
+
+interface UseDeriveValueParams {
+  pair1: string;
+  pair2: string;
+  amount: string;
+}
+
+type SwapResponse = {
+  in: string;
+  out: string;
+};
+
+export const useDeriveValue = ({
+  pair1,
+  pair2,
+  amount,
+}: UseDeriveValueParams) => {
+  const { data } = useSWR(
+    !!pair1 && !!pair2 && !!amount ? { url: "/swap" } : null,
+    ({ url }) => {
+      return ky
+        .get(url, {
+          searchParams: {
+            from: pair1,
+            to: pair2,
+            amount,
+          },
+        })
+        .json<SwapResponse>();
+    }
+  );
+
+  return data?.out;
+};
