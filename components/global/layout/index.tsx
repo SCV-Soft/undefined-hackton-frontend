5889caa2ced85d354be74ea4e12759226e7a8aa9
2-NOW <kj109888@gmail.com>
2023-07-29 23:32:07 +0900

    chore: update sidebar import

--- components/global/layout/index.tsx
+++ components/global/layout/index.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 
-import { Side } from "./side";
+import { Side } from "./sidebar";
 
 export const GlobalLayout = ({ children }: PropsWithChildren) => {
   return (
