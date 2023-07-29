c9048c53e943813d0a381e2b7f1e2d9b6ed2a385
2-NOW <kj109888@gmail.com>
2023-07-30 01:47:48 +0900

    feat: Update AstarPage header title and subtitle

--- app/astar/page.tsx
+++ app/astar/page.tsx
@@ -35,20 +35,19 @@ const AstarPageContent = ({
 export default function AstarPage({ searchParams }: AstarPageProps) {
   const { pair1, pair2 } = searchParams ?? {};
 
-  // TODO: set header title and subtitle
   return (
     <div className="flex flex-col gap-4">
       <Header
         title={
           <div className="flex items-end gap-4">
-            <span>Layer 1 Swap</span>
+            <span>Astar Swap</span>
             <span className="flex items-center gap-2 text-lg">
-              vETH
-              <FaSync />
+              v{pair1?.toUpperCase()}
+              <FaSync />v{pair2?.toUpperCase()}
             </span>
           </div>
         }
-        subtitle={`Swap to vETH the next generation LSD Token`}
+        subtitle={`Swap ${pair1?.toUpperCase()} to v${pair2?.toUpperCase()} the next generation LSD Token`}
       />
       <AstarPageContent {...{ pair1, pair2 }} />
     </div>
