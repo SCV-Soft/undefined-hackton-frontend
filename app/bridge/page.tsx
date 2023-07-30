f468041a1459ebfc80027c45fe6b5f0502c4ba35
2-NOW <kj109888@gmail.com>
2023-07-30 14:08:27 +0900

    refactor: Simplify BridgePage component and add missing header

--- app/bridge/page.tsx
+++ app/bridge/page.tsx
@@ -1,27 +1,48 @@
 import { redirect } from "next/navigation";
+import { FaArrowRight } from "react-icons/fa";
 
 import { CosmosAstarSwap } from "components/app/bridge/cosmos-astar";
 import { ETHAstarSwap } from "components/app/bridge/eth-astar";
 import { ETHPolygonSwap } from "components/app/bridge/eth-polygon";
+import { Header } from "components/common";
 
-export default function BridgePage({
-  searchParams,
-}: {
-  searchParams: {
-    pair1: string;
-    pair2: string;
-  };
-}) {
-  const { pair1, pair2 } = searchParams || {};
+interface Pairs {
+  pair1: string;
+  pair2: string;
+}
 
+const BridgePageContent = ({ pair1, pair2 }: Pairs) => {
   switch (`${pair1}-${pair2}`) {
-    case "atom-astar":
+    case "atom-astr":
       return <CosmosAstarSwap />;
-    case "eth-astar":
+    case "eth-astr":
       return <ETHAstarSwap />;
-    case "eth-polygon":
+    case "eth-dot":
       return <ETHPolygonSwap />;
     default:
-      return redirect("/bridge?pair1=eth&pair2=astar");
+      return redirect("/bridge?pair1=eth&pair2=astr");
   }
+};
+
+export default function BridgePage({ searchParams }: { searchParams: Pairs }) {
+  const { pair1, pair2 } = searchParams || {};
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Header
+        title={
+          <div className="flex items-end gap-4">
+            <span>Bridge</span>
+            <span className="flex items-center gap-2 text-lg">
+              {`v${pair1?.toUpperCase()}`}
+              <FaArrowRight />
+              {`v${pair2?.toUpperCase()}`}
+            </span>
+          </div>
+        }
+        subtitle="Move your vETH to Layer 2 by one step"
+      />
+      <BridgePageContent {...{ pair1, pair2 }} />
+    </div>
+  );
 }
